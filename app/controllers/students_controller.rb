4f52eca4261793c5d94d99ea9bfe788466ae3844
Joy Lin <vvlnote@hotmail.com>
2019-05-20 22:30:51 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -14,6 +14,6 @@ class StudentsController < ApplicationController
   
   def create
     Student.create(:first_name => params[:student][:first_name], :last_name => params[:student][:last_name])
-    redirect_to posts_path
+    redirect_to students_path
   end
 end
